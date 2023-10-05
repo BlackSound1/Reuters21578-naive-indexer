b17cc27ffe0f31028eecf4e0ea9d8229c0fa57b8
BlackSound1 <matthewr@videotron.ca>
2023-10-05 09:14:05 -0400

    Should have working subproject3. Takes hours to finish

--- subproject3.py
+++ subproject3.py
@@ -2,6 +2,7 @@ import json
 from collections import defaultdict
 
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 
 def main():
@@ -13,13 +14,23 @@ def main():
 
     # Remove numbers
     index = remove_numbers(index)
-
     print(f"Length after removing numbers: {len(index)}")
 
     # Do case folding
     index = case_folding(index)
-
     print(f"Length after case folding: {len(index)}")
+
+    # Remove 30 stopwords
+    index30 = stopwords30(index)
+    print(f"Length after removing 30 stopwords (from the case-folded index): {len(index30)}")
+
+    # Remove 150 stopwords
+    index150 = stopwords150(index)
+    print(f"Length after removing 150 stopwords (from the case-folded index): {len(index150)}")
+
+    # Stem
+    index = stem(index150)
+    print(f"Length after stemming (the index after removing 150 stopwords): {len(index)}")
 
 
 def remove_numbers(index: dict) -> dict:
@@ -43,7 +54,7 @@ def case_folding(index: dict) -> dict:
 
     # For each of the keys in the index, associate members of the same word type and put them in the similar_keys set
     print("Making a set of tuples. Each tuple contains all versions of a word type, considering capitalization."
-          "\nThis will take a while...")
+          " This will take a while...")
     for key in index_keys:
         similar = tuple(k for k in index_keys if k.lower() == key.lower())
         similar_keys.add(similar)
@@ -108,7 +119,50 @@ def stopwords150(index: dict) -> dict:
 
 
 def stem(index: dict) -> dict:
-    pass
+    index_keys = index.keys()
+
+    # Create them Porter stemmer
+    stemmer = PorterStemmer()
+
+    new_index = defaultdict(list)
+
+    # Create a tuple of keys that are similar, where similar means of the same word type, considering stemming
+    similar_keys = set()
+
+    # For each of the keys in the index, associate members of the same word type and put them in the similar_keys set
+    print("Making a set of tuples. Each tuple contains all versions of a word type, considering stemming."
+          " This will take a while...")
+    for key in index_keys:
+        similar = tuple(k for k in index_keys if stemmer.stem(k) == stemmer.stem(key))
+        similar_keys.add(similar)
+
+    # Navigate through each of the word tuples
+    for word_tuple in similar_keys:
+        # Get the stemmed version of the word type
+        normalized = stemmer.stem(word_tuple[0])
+
+        # Create a set of postings to add. It will contain 1 copy of all postings from each of the instances of this
+        # word type
+        new_postings = set()
+
+        # Go through each of the keys associated with this word type
+        for word in word_tuple:
+            # Add, to the set of new postings, the postings list from the old index for this word
+            new_postings.update(index[word])
+
+        # Create a new key for the new index based on the stemmed version of the word type.
+        # This key should be associated with the sorted version of the list of new postings
+        new_index[normalized] = sorted(list(new_postings))
+
+    # Sort index by keys
+    new_index = dict(sorted(new_index.items()))
+
+    print("Saving to file: output/5. stemmed_index.txt")
+
+    with open("output/5. stemmed_index.txt", "wt") as f:
+        json.dump(new_index, f)
+
+    return new_index
 
 
 if __name__ == '__main__':
